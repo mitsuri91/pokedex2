2ca426ed9b347dde3dce9007c59d48194d206f0f
mitsuri91 <launaymaxime5@gmail.com>
2025-01-02 12:06:55 +0100

    add component dynamic

--- src/components/PokemonCar.tsx
+++ src/components/PokemonCar.tsx
@@ -1,12 +1,27 @@
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
 function PokemonCard() {
+	const pokemon = pokemonList[0];
 	return (
-		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="Bulbasaur"
-			/>
-			<figcaption>Bulbasaur</figcaption>
-		</figure>
+		<>
+			{pokemon.imgSrc ? (
+				<figure>
+					<img src={pokemon.imgSrc} alt={pokemon.name} />
+					<figcaption>{pokemon.name}</figcaption>
+				</figure>
+			) : (
+				<p>???</p>
+			)}
+		</>
 	);
 }
 export default PokemonCard;
